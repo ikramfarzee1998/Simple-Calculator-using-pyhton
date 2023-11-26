0ecb3bc3f4d5aefb69abd58c53971fa8052b60f1
ikramfarzee1998 <ikramfarzee1998@gmail.com>
2023-11-26 21:26:48 +0800

    update calculator function

--- Calculator/Calculator/Calculator.py
+++ Calculator/Calculator/Calculator.py
@@ -1,32 +1,41 @@
 def calculator():
     operator = input("Please enter an operator (+ - * /): ")
-    num1 = float(input("1st number: "))
-    num2 = float(input("2nd number: "))
-
     if operator == "+":
+        num1,num2=inputnum()
         result = num1 + num2
         print(round(result, 3))
         # this is for addition operation
 
     elif operator == "-":
+        num1,num2=inputnum()
         result = num1 - num2
         print(round(result, 3))
         # this is for subtraction operation
 
     elif operator == "*":
+        num1,num2=inputnum()
         result = num1 * num2
         print(round(result, 3))
         # this is for multiplication operation
 
     elif operator == "/":
+        num1,num2=inputnum()
         result = num1 / num2
         print(round(result, 3))
         # this is for division operation
 
     else:
         print(f"{operator} is not a valid operator")
-        return  
+        return
+        #if input wrong operator it will end the loop
+
+def inputnum():
+    
+    num1 = float(input("1st number: "))
+    num2 = float(input("2nd number: "))
+    return num1,num2
 
 
-calculator()
+while True:
+    calculator()
 
